8daf4cc59c692759b57c239b0780a0acc6624f3c
Henrique Richter <henrique-richter@hotmail.com>
2023-05-06 10:56:40 -0300

    Adding work in progress to toggle the feature and load dinamically the files

---
+++ scripts/addstyle.js
@@ -0,0 +1,8 @@
+var style = document.createElement('style');
+style.innerHTML = `
+.kjWKTd {
+    filter: blur(3px) grayscale(1);
+}
+`;
+console.log(frames)
+frames['gtn-roster-iframe-id'].document.head.appendChild(style);
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+chrome.tabs.query({active: true}, function(tabs){
+    chrome.scripting.insertCSS({
+        target: { tabId: tabs[1].id, allFrames : true },
+        files: ["style.css"]
+        });
+})
